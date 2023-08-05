a88ec62ac31cf542f544f290725debb8e6f12d82
Daniel Amaya <daniel_amaya7398@hotmail.com>
2023-08-04 19:45:24 -0500

    build(config): first config
    [x] First configuration for the system
    [x] First configuration for the admin-personal API

---
+++ README.md
@@ -0,0 +1,1 @@
+# sistema historial clinico
---
+++ administrativo/src/test/java/edu/uady/administrativo/AdministrativoApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.uady.administrativo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdministrativoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
