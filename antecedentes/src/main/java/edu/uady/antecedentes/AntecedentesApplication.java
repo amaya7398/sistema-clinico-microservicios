dbc8ea4d7a94eb3903e6d64a3cfabae51ed902d4
Daniel Amaya <daniel_amaya7398@hotmail.com>
2023-08-06 20:32:01 -0500

    build(api-2): Config for api "antecedentes"
    [x] First configuration for the antecedentes-personal API
    * Dependecies
    * Port config
    * Context-path

---
+++ antecedentes/src/main/java/edu/uady/antecedentes/AntecedentesApplication.java
@@ -0,0 +1,13 @@
+package edu.uady.antecedentes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntecedentesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AntecedentesApplication.class, args);
+	}
+
+}
---
+++ antecedentes/src/test/java/edu/uady/antecedentes/AntecedentesApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.uady.antecedentes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntecedentesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
